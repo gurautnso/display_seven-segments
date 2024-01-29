21b5756e1f4543b5905bb7fdfa634547157ed434
Guilherme Simões Rosa <133579859+gurautnso@users.noreply.github.com>
2024-01-28 21:56:41 -0300

    base code

---
+++ project/lib/__init__.py
@@ -0,0 +1,11 @@
+def numbers():  # um dicionario com cada key dendo uma lista espeficicando quais segmentos devem ser ligados
+    Numbers = {1: [2, 5],
+               2: [0, 2, 3, 4, 6],
+               3: [0, 2, 3, 5, 6],
+               4: [1, 2, 3, 5],
+               5: [0, 1, 3, 5, 6],
+               6: [0, 1, 3, 4, 5, 6],
+               7: [0, 1, 2, 5],
+               8: [0, 1, 2, 3, 4, 5, 6],
+               9: [0, 1, 2, 3, 5, 6]}
+    return Numbers
